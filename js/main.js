16c3a2e4a939df985e4a410fd47dd6349898c50d
iustin <iustin.nitza@gmail.com>
2018-05-27 19:26:05 +0200

    add player and animations

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
-let platforms;
+let platforms, player;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -42,6 +42,29 @@ function create() {
   platforms.create(750, 220, 'ground');
   // this.add.image(400, 300, 'star');
 
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4}],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 
 
 }
